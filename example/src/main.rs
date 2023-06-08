14de630e64ff3e534f4412a659466b0bdc32b680
Landon Moon <lankm@live.com>
2023-06-08 00:29:28 -0500

    learning collections inside and out

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,12 +1,16 @@
 #![allow(unused)]
 
-use std::collections::{HashMap, BinaryHeap};
+// https://doc.rust-lang.org/std/collections/index.html
+use std::collections::{ BTreeMap, BTreeSet, BinaryHeap, VecDeque, LinkedList, HashMap, HashSet };
 
 fn main() {
     let vec = vec![1,2,5,4];
-    let heap = BinaryHeap::from(vec);
+    let vec_deque = VecDeque::from([1,2,3]);
+    let bin_tree_set = BTreeSet::from([0,4,2,6,-5]);
+    let bin_heap = BinaryHeap::from([0,5,2,8,2,6,4,9]);
 
-    for n in heap.iter() {
-        println!("{n}");
-    }
+    let c = bin_tree_set.into_iter();
+    // for n in bin_tree_set.iter() {
+    //     println!("{n}");
+    // }
 }
