c582603454dfd97990d1fd64a4ae5c9ce82fae22
Landon Moon <lankm@live.com>
2023-06-08 23:12:39 -0500

    More leetcode stuff

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,13 +4,12 @@
 use std::collections::{ BTreeMap, BTreeSet, BinaryHeap, VecDeque, LinkedList, HashMap, HashSet };
 
 fn main() {
-    let vec = vec![1,2,5,4];
-    let vec_deque = VecDeque::from([1,2,3]);
-    let bin_tree_set = BTreeSet::from([0,4,2,6,-5]);
-    let bin_heap = BinaryHeap::from([0,5,2,8,2,6,4,9]);
-
-    let c = bin_tree_set.into_iter();
-    // for n in bin_tree_set.iter() {
-    //     println!("{n}");
-    // }
+    search_insert(vec![0,2,3,4,6,7,8,9,20], 15);
+}
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+    let i = nums.binary_search(&target);
+    match i {
+        Ok(x) => x as i32,
+        Err(x) => x as i32,
+    }
 }
