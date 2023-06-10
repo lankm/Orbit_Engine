6724b76c008d99ef94cbbb9336eaea9009aa0ab7
Landon Moon <lankm@live.com>
2023-06-10 12:02:26 -0500

    branch testing

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,14 +2,14 @@
 
 // https://doc.rust-lang.org/std/collections/index.html
 use std::collections::{ BTreeMap, BTreeSet, BinaryHeap, VecDeque, LinkedList, HashMap, HashSet };
+use std::mem::size_of;
+
+struct Test {
+    x: i32,
+    y: i32,
+}
+
 
 fn main() {
-    search_insert(vec![0,2,3,4,6,7,8,9,20], 15);
-}
-pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-    let i = nums.binary_search(&target);
-    match i {
-        Ok(x) => x as i32,
-        Err(x) => x as i32,
-    }
+    println!("{}", size_of::<Test>());
 }
