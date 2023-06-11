e5f7a5a33bb22a61a2806d7130dd02b155dc5426
Landon Moon <lankm@live.com>
2023-06-11 16:35:13 -0500

    daily commit

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,22 +1,25 @@
 #![allow(unused)]
 
-use core::fmt;
 // https://doc.rust-lang.org/std/collections/index.html
 use std::collections::{ BTreeMap, BTreeSet, BinaryHeap, VecDeque, LinkedList, HashMap, HashSet };
 use std::mem::size_of;
+use std::fmt;
 
 struct Test {
-    x: i32,
-    y: i32,
+    x: i128,
 }
 impl fmt::Display for Test {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+        let power = 20;
+        let whole = self.x >> power;
+        let decimal = self.x - (whole << power);
+        write!(f, "{}.{:0>6}", whole, decimal)
     }
 }
 
 
 fn main() {
-    let s = Test{ x:2, y:5 };
+    let mut s = Test{ x:127984<<20 };
+    s.x -= 1;
     println!("{s}");
 }
