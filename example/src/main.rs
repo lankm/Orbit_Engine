dfaf046525649873b92ae679e97e3d91621f3e0e
Landon Moon <lankm@live.com>
2023-06-22 22:26:49 -0500

    Orbit Testing

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,25 +1,40 @@
 #![allow(unused)]
+#![allow(non_snake_case)]
 
 // https://doc.rust-lang.org/std/collections/index.html
 use std::collections::{ BTreeMap, BTreeSet, BinaryHeap, VecDeque, LinkedList, HashMap, HashSet };
 use std::mem::size_of;
-use std::fmt;
+use std::f32::consts::PI;
 
-struct Test {
-    x: i128,
-}
-impl fmt::Display for Test {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let power = 20;
-        let whole = self.x >> power;
-        let decimal = self.x - (whole << power);
-        write!(f, "{}.{:0>6}", whole, decimal)
+fn main() {
+    const COUNT: i32 = 1000;
+    const STEP: f32 = (2.0*PI/(COUNT as f32));
+
+    for i in 0..COUNT {
+        let M = STEP*(i as f32);
+        let pos = calc_pos(M, 2.0, 1.0);
+        println!("({}, {})", pos.0, pos.1);
     }
 }
 
+fn calc_E(M: f32, e: f32) -> f32 {
+    let mut E: f32 = 0.0;
 
-fn main() {
-    let mut s = Test{ x:127984<<20 };
-    s.x -= 1;
-    println!("{s}");
+    for i in 0..1000 {
+        E = M + e*E.sin();
+    }
+
+    return E;
+}
+fn calc_e(a: f32, b: f32) -> f32 {
+    let e = (1.0-((b*b)/(a*a))).sqrt();
+    return e;
+}
+fn calc_pos(M: f32, a: f32, b: f32) -> (f32, f32) {
+    let e = calc_e(a, b);
+    let E = calc_E(M, e);
+
+    let x = a*(E.cos()-e);
+    let y = b*E.sin();
+    return ( x, y );
 }
